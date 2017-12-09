919679fe6bca46a8207c2104361206365cf0a43b
Ante <vuletic23@gmail.com>
2017-12-09 14:03:13 +0100

    Add simple navigation bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import {NgModule} from '@angular/core';
 
-
+import {environment} from '../environments/environment';
+import {CoreModule} from './core/modules/core.module';
 import {ServiceWorkerModule} from '@angular/service-worker';
 import {AppComponent} from './app.component';
 
-import {environment} from '../environments/environment';
-import {CoreModule} from './core/modules/core.module';
+import {NavigationComponent} from './components/navigation/navigation.component';
+import {MaterialComponentsModule} from './core/modules/material-components.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponent,
   ],
   imports: [
     CoreModule,
+    MaterialComponentsModule,
     ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
   providers: [],
--- src/app/core/modules/material-components.module.ts
+++ src/app/core/modules/material-components.module.ts
@@ -1,15 +1,21 @@
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports:
     [
       MatButtonModule,
+      MatToolbarModule,
+      MatIconModule
     ],
   exports:
     [
-      MatButtonModule
+      MatButtonModule,
+      MatToolbarModule,
+      MatIconModule
     ],
 })
-export class AngularMaterialComponentsModule {
+export class MaterialComponentsModule {
 }
