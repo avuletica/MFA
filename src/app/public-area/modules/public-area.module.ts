974e911c2d1852d6d3e392ce7ba9e7ef80a90922
Ante <vuletic23@gmail.com>
2017-12-10 16:14:37 +0100

    Remove news component

--- src/app/public-area/modules/public-area.module.ts
+++ src/app/public-area/modules/public-area.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import {PublicAreaRoutingModule} from './public-area-routing.module';
 import {LoginComponent} from '../components/login/login.component';
-import {NewsComponent} from '../components/news/news.component';
 import {ContactComponent} from '../components/contact/contact.component';
 import {HomeComponent} from '../components/home/home.component';
 
@@ -13,7 +12,6 @@ import {HomeComponent} from '../components/home/home.component';
   ],
   declarations: [
     HomeComponent,
-    NewsComponent,
     ContactComponent,
     LoginComponent,
   ]
