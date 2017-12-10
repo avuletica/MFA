85b3cee2f116142d9b2d50e5ae32f7b204bbd79f
Ante <vuletic23@gmail.com>
2017-12-10 12:02:47 +0100

    Implement user model

---
+++ src/app/core/models/user.model.ts
@@ -0,0 +1,23 @@
+export class UserModel {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  username?: string;
+  password?: string;
+  email?: string;
+
+  constructor(id?: number,
+              firstName?: string,
+              lastName?: string,
+              username?: string,
+              password?: string,
+              email?: string) {
+
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.username = username;
+    this.password = password;
+    this.email = email;
+  }
+}
