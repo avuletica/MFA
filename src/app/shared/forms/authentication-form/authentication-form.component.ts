f3056dd5f3a01acc0125c6a5e874c3bf0277b252
avuletica <vuletic23@gmail.com>
2018-01-07 16:49:46 +0100

    add expander panel after first step of authentication

--- src/app/shared/forms/authentication-form/authentication-form.component.ts
+++ src/app/shared/forms/authentication-form/authentication-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-authentication-form',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./authentication-form.component.css']
 })
 export class AuthenticationFormComponent implements OnInit {
+  backupCode: number;
+  mobileToken: number;
+  smsCode: number;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+    this.backupCode = null;
+    this.mobileToken = null;
+    this.smsCode = null;
+  }
+
+  verifyBackupCode(): void {
+    console.log(this.backupCode);
+  }
+
+  verifyMobileToken(): void {
+    console.log(this.mobileToken);
+  }
+
+  verifySmsCode(): void {
+    console.log(this.smsCode);
   }
 
 }
