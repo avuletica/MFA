d4966879dbdede79c76c5d0b7e9e4b39b160dceb
avuletica <vuletic23@gmail.com>
2018-01-10 10:17:37 +0100

    replace hardcoded strings

--- src/app/core/services/user/user.service.ts
+++ src/app/core/services/user/user.service.ts
@@ -2,18 +2,17 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
 import {UserModel} from '../../models/user.model';
+import {endpoints} from '../../utils/endpoints';
 
 @Injectable()
 export class UserService {
-  readonly restServiceApiUrl = 'http://localhost:8080';
-  readonly getUserInformationEndpoint = '/api/security/user';
 
   constructor(private http: HttpClient) {
 
   }
 
   public getUserInformation(username: string): Observable<UserModel> {
-    return this.http.get<UserModel>(this.restServiceApiUrl + this.getUserInformationEndpoint + '/' + username);
+    return this.http.get<UserModel>(endpoints().auth.user + '/' + username);
   }
 
 }
