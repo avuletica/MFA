046275c1300a5eaaa82fa8dcc6eb7f39606e684c
Ante <vuletic23@gmail.com>
2018-03-08 17:45:01 +0100

    implement form @ secret question component

--- src/app/public-area/components/two-step-verification/secret-question/secret-question.component.ts
+++ src/app/public-area/components/two-step-verification/secret-question/secret-question.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {NotificationService} from '../../../../core/services/notification/notification.service';
+import {Router} from '@angular/router';
+import {UserService} from '../../../../core/services/user/user.service';
+import {AuthService} from '../../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-secret-question',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./secret-question.component.css']
 })
 export class SecretQuestionComponent implements OnInit {
+  secretQuestionFormGroup: FormGroup;
+  inputSecretQuestion: string;
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private userService: UserService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              public notificationService: NotificationService) {
+  }
 
   ngOnInit() {
+    this.inputSecretQuestion = '';
+    this.secretQuestionFormGroup = this.formBuilder.group({
+      inputSecretQuestion: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+
+
   }
 
 }
